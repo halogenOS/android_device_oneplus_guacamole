fb3f1023c39ce3df5512382358d2c2ed8944fd05
Simao Gomes Viana <simao@halogenos.org>
2022-06-23 21:13:28 +0200

    XOSify

---
+++ aosp_guacamole.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
+$(call inherit-product, device/oneplus/guacamole/device.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, $(CUSTOM_VENDOR_DIR)/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_guacamole
+PRODUCT_DEVICE := guacamole
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := GM1917
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+
+# Build info
+BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:11/RKQ1.201022.002/2106290952:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=OnePlus7Pro \
+    PRODUCT_NAME=OnePlus7Pro \
+    PRIVATE_BUILD_DESC="OnePlus7Pro-user 11 RKQ1.201022.002 2106290952 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
